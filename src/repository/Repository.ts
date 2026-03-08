6edff870d74b1940c6cdf9e21922431f00f55f5b
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-03-07 23:54:15 -0300

    Correção no repository

--- src/repository/Repository.ts
+++ src/repository/Repository.ts
@@ -1,6 +1,6 @@
 export interface Repository<createT, updateT, responseT> {
     create(entity: createT): Promise<responseT>;
-    uptade(id: string, entity: updateT): Promise<responseT>;
+    update(id: string, entity: updateT): Promise<responseT>;
     findAll(): Promise<responseT[]>;
     findById(id: string): Promise<responseT | null>;
     delete(id: string): Promise<void>;
