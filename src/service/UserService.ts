15a311a7a7ae623996d75a6ac79b0fce8a8d126e
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-03-08 16:24:11 -0300

    user service com login e cadastro

---
+++ src/service/UserService.ts
@@ -0,0 +1,31 @@
+import { UserResponseDtoSchema, type CreateUserDTO, type LoginUserDTO, type UserResponseDTO } from "../model/User.js";
+import type { UserRepository } from "../repository/implementations/UserRepository.js";
+import { hash, compare } from "bcrypt";
+import "dotenv/config"
+import { generateToken } from "../utils/TokenService.js";
+
+export class UserService {
+    constructor(private readonly userRepository: UserRepository) {}
+
+    async signUpUser(user: CreateUserDTO) {
+        const { password } = user
+        const hashPassword = await hash(password, 12);
+        user.password = hashPassword;
+        const userCreated = await this.userRepository.create(user)
+        return UserResponseDtoSchema.parse(userCreated)
+    }
+
+    async signInUser(user: LoginUserDTO) {
+        const { email, password } = user
+        const userExists = await this.userRepository.findByEmail(email)
+        if (!userExists) {
+            throw new Error("User não existe")
+        }
+        const passwordCorrect = await compare(password, userExists.password);
+        if (!passwordCorrect) {
+            throw new Error("Email ou senha incorretos")
+        }
+        const token = generateToken(userExists.id)
+        return token
+    }
+}
