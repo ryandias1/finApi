e5afa2bd03cb0e042342ee2f9e08b25c27e3b44b
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-03-08 17:17:19 -0300

    server usando rotas

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import "dotenv/config"
+import { userRoutes } from "./routes/UserRoutes.js"
 
 const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
+app.use("/users", userRoutes)
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
