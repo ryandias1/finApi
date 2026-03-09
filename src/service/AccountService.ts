37278d119b2b1ce071583168fcf5bbf69a8772ec
ryandias1 <ryan.cd@puccampinas.edu.br>
2026-03-08 23:37:14 -0300

    Service de extrato

---
+++ src/service/AccountService.ts
@@ -0,0 +1,18 @@
+import { AccountWithOperationsSchema, type accountWithOperationsDTO } from "../model/Account.js";
+import type { AccountRepository } from "../repository/implementations/AccountRepository.js";
+
+export class AccountService {
+    constructor(private readonly accountRepository: AccountRepository) {}
+
+    async getStatement(id: string): Promise<accountWithOperationsDTO> {
+        const allOperations = await this.accountRepository.getAccountStatement(id)
+        if (!allOperations) throw new Error("Conta não encontrada")
+        return AccountWithOperationsSchema.parse(allOperations)
+    }
+
+    async getAccountByUser(id: string) {
+        const user = await this.accountRepository.findByUser(id)
+        if (!user) throw new Error("Usuario não existe")
+        return user.id
+    }
+}
